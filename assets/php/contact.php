54aa6455e9a73d3e60ba14d821f41dcfc3a50274
Richmond Frimpong <richee305@yahoo.ca>
2016-03-24 10:51:20 -0400

    Updated email form

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,33 +1,35 @@
 <?php
+$host  = $_SERVER['HTTP_HOST'];
+$headers =  'MIME-Version: 1.0' . "\r\n";
+$headers .= 'From: Your name <info@address.com>' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .='X-Mailer: PHP/' . phpversion();
 
-if(isset($_POST['message'])){
-
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$message = $_POST['message'];
-    
-	
-	$to      = 'you@example.com';
+if($_GET['c_name'] || $_GET['c_email'] || $_GET['c_message']){
+	$name = $_GET['c_name'];
+	$email = $_GET['c_email'];
+	$message = $_GET['c_message'];
+}
+	$to      = 'rboy879@gmail.com';
 	$subject = 'Site Contact Form';
-
-	$headers = 'From: '. $email . "\r\n" .
-    'Reply-To: '. $email . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-	$status = mail($to, $subject, $message, $headers);
-
+    $message = str_replace("\n.", "\n..", $message);
+    $message .= "\r\n Sender\t";
+    $message .= $email;
+    $status = mail($to, $subject, $message , $headers);
 	if($status == TRUE){	
 		$res['sendstatus'] = 'done';
-	
 		//Edit your message here
 		$res['message'] = 'Form Submission Successful';
+        //$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+        //$extra = 'index.php';
+        //header("Location: http://$host$uri/$extra");
+        header("Location: http://$host//MyWeb");
+        //exit;
     }
 	else{
-		$res['message'] = 'Failed to send mail. Please mail me to you@example.com';
+		$res['message'] = 'Failed to send mail. Please mail me to you@example.com\n';
+        echo json_encode($res);
+        header("Location: http://$host//MyWeb");
 	}
 	
-	
-	echo json_encode($res);
-}
-
 ?>
