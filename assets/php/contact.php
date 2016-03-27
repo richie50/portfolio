50b4e2ec5bbdb8d3e5142f23409f3a6384391123
Richmond Frimpong <richee305@yahoo.ca>
2016-03-27 14:15:57 -0400

    Bug fixed deployment

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,9 +1,9 @@
 <?php
 echo "Hello World";
-$host  = "www.google.ca"
-$headers =  'MIME-Version: 1.0' . "\r\n";
-$headers .= 'From: Your name <info@address.com>' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+//$host  = 'www.google.ca'
+//$headers =  'MIME-Version: 1.0' . "\r\n";
+//$headers .= 'From: Your name <info@address.com>' . "\r\n";
+//$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 $headers .='X-Mailer: PHP/' . phpversion();
 
 if($_GET['c_name'] || $_GET['c_email'] || $_GET['c_message']){
