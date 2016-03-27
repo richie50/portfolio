cb0e7e960c023f958e43735bf1010e47427cac91
Richmond Frimpong <richee305@yahoo.ca>
2016-03-27 14:28:51 -0400

    Bug fixed deployment

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,11 +1,17 @@
 <?php
-echo "Hello World";
-$host  = 'richmondfrimpong.herokuapp.com/'
-$headers =  'MIME-Version: 1.0' . '\n';
-$headers .= 'From: Your name <info@address.com>' . '\n';
-$headers .= 'Content-type: text/html; charset=iso-8859-1'.'\n';
+function debug_to_console($data) {
+    if(is_array($data) || is_object($data))
+	{
+		echo("<script>console.log('PHP: ".json_encode($data)."');</script>");
+	} else {
+		echo("<script>console.log('PHP: ".$data."');</script>");
+	}
+	debug_to_console("Hello World");
+$headers =  'MIME-Version: 1.0 \n';
+$headers .= 'From: Your name <info@address.com> \n';
+$headers .= 'Content-type: text/html; charset=iso-8859-1 \n';
 $headers .='X-Mailer: PHP/' . phpversion();
-
+debug_to_console($headers);
 if($_GET['c_name'] || $_GET['c_email'] || $_GET['c_message']){
 	$name = $_GET['c_name'];
 	$email = $_GET['c_email'];
@@ -25,13 +31,14 @@ if($_GET['c_name'] || $_GET['c_email'] || $_GET['c_message']){
         //$extra = 'index.php';
         //echo $uri."/".$extra; 
         //header("Location: http://$host$uri/$extra");
-        header("Location: https://$host");
+        header("Location: https://richmondfrimpong.herokuapp.com/");
         //exit;
     }
 	else{
 		$res['message'] = 'Failed to send mail. Please mail me to you@example.com\n';
         
-        	header("Location: https://$host");
+        	header("Location: https://richmondfrimpong.herokuapp.com/");
 	}
 	echo json_encode($res);
+}
 ?>
