58ff0345e29a06fee917e69922b5a5b6fb0411ee
Richmond Frimpong <richee305@yahoo.ca>
2016-03-27 14:22:42 -0400

    Bug fixed deployment

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,9 +1,9 @@
 <?php
 echo "Hello World";
-//$host  = 'www.google.ca'
-//$headers =  'MIME-Version: 1.0' . "\r\n";
-//$headers .= 'From: Your name <info@address.com>' . "\r\n";
-//$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$host  = 'richmondfrimpong.herokuapp.com/'
+$headers =  'MIME-Version: 1.0' . '\n';
+$headers .= 'From: Your name <info@address.com>' . '\n';
+$headers .= 'Content-type: text/html; charset=iso-8859-1'.'\n';
 $headers .='X-Mailer: PHP/' . phpversion();
 
 if($_GET['c_name'] || $_GET['c_email'] || $_GET['c_message']){
@@ -25,13 +25,13 @@ if($_GET['c_name'] || $_GET['c_email'] || $_GET['c_message']){
         //$extra = 'index.php';
         //echo $uri."/".$extra; 
         //header("Location: http://$host$uri/$extra");
-        header("Location: https://www.google.ca");
+        header("Location: https://$host");
         //exit;
     }
 	else{
 		$res['message'] = 'Failed to send mail. Please mail me to you@example.com\n';
         
-        	header("Location: https://www.google.ca");
+        	header("Location: https://$host");
 	}
 	echo json_encode($res);
 ?>
